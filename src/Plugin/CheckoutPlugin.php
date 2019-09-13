6d64408f2def16988eb801a54243173430b6c5e3
Gabriel Moreira <gabriel.moreira@fotomerchant.com>
2019-09-13 23:51:26 +1000

    Fixed stripe connect account parameter.

--- src/Plugin/CheckoutPlugin.php
+++ src/Plugin/CheckoutPlugin.php
@@ -157,8 +157,8 @@ class CheckoutPlugin extends AbstractPlugin
             $parameters['stripeVersion'] = $data->get('stripeVersion');
         }
 
-        if ($data->has('connectedStripeAccount')) {
-            $parameters['connectedStripeAccountHeader'] = $data->get('connectedStripeAccount');
+        if ($data->has('connectedStripeAccountHeader')) {
+            $parameters['connectedStripeAccountHeader'] = $data->get('connectedStripeAccountHeader');
         }
 
         if ($data->has('metadata')) {
